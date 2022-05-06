0c158fe57bbb3eed0a579abd1f759068bf8acb5b
MILKPlease <lukescrivens444@gmail.com>
2022-05-06 21:33:53 +1000

    Added grid with functioning hover

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const containerDiv = document.querySelector("#container");
-let gridCount = 0;
 
-while (gridCount < 32) {
-  const gridDivs = document.createElement("div");
-  gridDivs.classList.add("content");
-  gridDivs.textContent = "HELLO!";
-  containerDiv.appendChild(gridDivs);
-  gridCount++;
-  console.log(gridCount);
+function makeGrid(rows, columns) {
+  //check for existing button to remove if played already
+  while (document.querySelector("button") !== null) {
+    document.querySelector("button").remove();
+  }
+
+  // create the grid
+  containerDiv.style.setProperty("--grid-rows", rows);
+  containerDiv.style.setProperty("--grid-columns", columns);
+  containerDiv.style.width = "960px";
+  containerDiv.style.overflow = "hidden";
+  for (i = 0; i < rows * columns; i++) {
+    let square = document.createElement("div");
+    square.style.minHeight = "0";
+    square.style.minWidth = "0";
+    square.style.overflow = "hidden";
+    containerDiv.appendChild(square).className = "grid-item";
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+  }
+  createButton();
+  // fillSquares();
 }
+
+function createButton() {}
+
+makeGrid(16, 16);
