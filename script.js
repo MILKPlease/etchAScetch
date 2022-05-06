e99c62afa80a0c62de830968209096e69dc43d75
MILKPlease <lukescrivens444@gmail.com>
2022-05-06 21:59:15 +1000

    Added function to change grid size by input

--- script.js
+++ script.js
@@ -25,6 +25,27 @@ function makeGrid(rows, columns) {
   // fillSquares();
 }
 
-function createButton() {}
+function createButton() {
+  const buttonDiv = document.querySelector("#buttonDiv");
+  const resetButton = document.createElement("button");
+  resetButton.textContent = "Reset Grid!";
+  resetButton.style.margin = "20px";
+  buttonDiv.appendChild(resetButton);
+
+  // add event listener to button and prompt user/reset grid/throw error > 100
+  resetButton.addEventListener("click", () => {
+    document.querySelectorAll(".grid-item").forEach((e) => e.remove());
+    let userGridInput = prompt(
+      "Please enter the number of grid squares per side (Max: 100): "
+    );
+    if (userGridInput > 100) {
+      alert("ERROR! You specified a grid size larger than 100.");
+      return;
+    }
+    rows = userGridInput;
+    columns = userGridInput;
+    makeGrid(rows, columns);
+  });
+}
 
 makeGrid(16, 16);
